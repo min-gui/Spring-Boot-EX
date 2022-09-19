4f0ac58a30028203beb6540f1495e5c30f585294
min-gui <msj349911@gmail.com>
2022-09-19 13:57:55 +0900

    add jdbc update, delete

--- src/test/java/com/example/springdb/repository/MemberRepositoryV0Test.java
+++ src/test/java/com/example/springdb/repository/MemberRepositoryV0Test.java
@@ -6,8 +6,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -17,12 +19,22 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberv3", 10000);
+        Member member = new Member("member10", 10000);
         repository.save(member);
         
         //findById
         Member findMember = repository.findById(member.getMemberId());
         System.out.println("findMember = " + findMember);
         assertThat(findMember).isEqualTo(member);
+
+        //update: money : 10,000 -> 20,000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
