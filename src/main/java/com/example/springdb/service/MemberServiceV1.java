c7a2a2195d5898357083106990504d9ac7af9165
min-gui <msj349911@gmail.com>
2022-10-12 01:42:01 +0900

    add memberService2

--- src/main/java/com/example/springdb/service/MemberServiceV1.java
+++ src/main/java/com/example/springdb/service/MemberServiceV1.java
@@ -15,14 +15,16 @@ public class MemberServiceV1 {
     계좌이체 로직
      */
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        //시작
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
         memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
-    }
 
+        //커밋, 롤백
+    }
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")){
             throw new IllegalStateException("이체중 예외 발생");
