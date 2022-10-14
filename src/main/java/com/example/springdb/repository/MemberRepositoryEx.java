92c05bf411cbef0f4d8c8ad24451b61697a54bbb
min-gui <msj349911@gmail.com>
2022-10-14 14:51:05 +0900

    키 중복 오류 에러 처리 추가

---
+++ src/main/java/com/example/springdb/repository/MemberRepositoryEx.java
@@ -0,0 +1,12 @@
+package com.example.springdb.repository;
+
+import com.example.springdb.domain.Member;
+
+import java.sql.SQLException;
+
+public interface MemberRepositoryEx {
+    Member save(Member member) throws SQLException;
+    Member findById(String memberId) throws SQLException;
+    void update(String memberId, int money) throws SQLException;
+    void delete(String memberId) throws SQLException;
+}
