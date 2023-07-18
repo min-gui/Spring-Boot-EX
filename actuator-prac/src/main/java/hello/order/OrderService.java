2087dc206b7759062b572d9aa04bb852c6df777a
min-gui <msj349911@gmail.com>
2023-07-19 00:29:34 +0900

    v0 버전 메트릭 등록

--- actuator-prac/src/main/java/hello/order/OrderService.java
+++ actuator-prac/src/main/java/hello/order/OrderService.java
@@ -5,5 +5,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public interface OrderService {
     void order();
     void cancel();
+    //멀티 스레드 상황에서 안전하게 값들을 증가 감소 시킬 수 있다.
     AtomicInteger getStock();
 }
