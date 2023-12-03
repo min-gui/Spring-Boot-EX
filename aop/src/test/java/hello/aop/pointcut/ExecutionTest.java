b5dda9ab69b1f84da47d0c76ad8e5ebe69a84f6a
min-gui <msj349911@gmail.com>
2023-12-03 14:34:09 +0900

    custom annotation

---
+++ aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -0,0 +1,36 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ExecutionTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+       helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+    
+    @Test
+    public void printMethod() throws Exception {
+        //execution( * ..package..Class)
+        //given
+        log.info("helloMethod={}", helloMethod);
+        
+        //when
+        
+        //then
+     
+     }
+        
+    
+
+}
