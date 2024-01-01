529f4b0259e16e6c163bce1193f57c21bbe1c8ca
min-gui <msj349911@gmail.com>
2024-01-01 22:29:00 +0900

    jdk cglib proxy 차이

---
+++ aop/src/test/java/hello/aop/ProxyCastingTest.java
@@ -0,0 +1,45 @@
+package hello.aop;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+public class ProxyCastingTest {
+
+    @Test
+    public void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false);
+
+        //프록시를 인터페이스 캐스팅 성공
+        //given
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        //JDK 동적 프록시를 구현 클래스로 캐스팅 시도 실패
+        //when
+        Assertions.assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl memberService = (MemberServiceImpl) memberServiceProxy;
+        });
+        //then
+    }
+
+    @Test
+    public void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+
+        //프록시를 인터페이스 캐스팅 성공
+        //given
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        //CGLIB 동적 프록시를 구현 클래스로 캐스팅 시도 성공
+        //when
+        MemberServiceImpl memberService = (MemberServiceImpl) memberServiceProxy;
+
+        //then
+    }
+}
