1c729187bce02d03237ddd7ca82caf40acf90d19
min-gui <msj349911@gmail.com>
2024-02-12 15:58:45 +0900

    기본 객체

--- mvc/thymeleaf-basic/src/main/java/hello/thymeleafbasic/controller/BasicController.java
+++ mvc/thymeleaf-basic/src/main/java/hello/thymeleafbasic/controller/BasicController.java
@@ -1,10 +1,15 @@
 package hello.thymeleafbasic.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@RequestMapping("/basic")
 public class BasicController {
 
     @GetMapping("/text-basic")
@@ -43,13 +49,32 @@ public class BasicController {
         return "basic/variable";
     }
 
+    //3.0 이상 기준
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request,
+                               HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        } }
+
+
     @Data
     static class User {
         private String username;
         private int age;
+
         public User(String username, int age) {
             this.username = username;
             this.age = age;
-        } }
+        }
+    }
 
 }
