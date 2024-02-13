7da67765b815d9a2fcced84f1af9f133d0e495b7
min-gui <msj349911@gmail.com>
2024-02-13 23:01:47 +0900

    url 링크

--- mvc/thymeleaf-basic/src/main/java/hello/thymeleafbasic/controller/BasicController.java
+++ mvc/thymeleaf-basic/src/main/java/hello/thymeleafbasic/controller/BasicController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -59,6 +60,20 @@ public class BasicController {
         model.addAttribute("servletContext", request.getServletContext());
         return "basic/basic-objects";
     }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param1", "data2");
+        return "basic/link";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
